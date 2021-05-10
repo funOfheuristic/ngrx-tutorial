60fc735c6bd307fe174a1fe75fb1a76d362ab64f
Fun Of Heuristic <40836348+funOfheuristic@users.noreply.github.com>
2021-05-11 00:51:44 +0530

    removing StoreDevtoolsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryService } from './Service/in-memory.service';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 // import { reducers, metaReducers } from './reducers';
 
@@ -25,10 +24,6 @@ import { environment } from '../environments/environment';
     MaterialModules,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryService),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
     // StoreModule.forRoot(reducers, {
     //   metaReducers,
     // }),
